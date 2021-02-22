d50cb6f7d6bdc24457e8708c73b8e96fdd6cfdf1
Olehandro767 <oleg.03.07.2000@gmail.com>
2021-02-22 22:56:08 +0200

    added android WDT project

---
+++ android operating system/WirelessDataTransmission_WDT/app/src/main/java/ua/edu/onaft/wirelessdatatransmission_wdt/SplashScreen.kt
@@ -0,0 +1,11 @@
+package ua.edu.onaft.wirelessdatatransmission_wdt
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        super.onCreate(savedInstanceState, persistentState)
+    }
+}
